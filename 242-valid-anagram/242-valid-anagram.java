5b05ed8c8a32ebea762f1a44aebbd22e2de4b50e
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-01-19 17:59:15 +0530

    Time: 138 ms (5.11%), Space: 53.9 MB (5.89%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        boolean ans=true;
+        ArrayList al=new ArrayList();
+        al.addAll(Arrays.asList(s.split("")));
+        ArrayList bl=new ArrayList();
+        bl.addAll(Arrays.asList(t.split("")));
+        Collections.sort(al);
+        Collections.sort(bl);
+        /*System.out.println(al + " " + bl);*/
+        if(al.equals(bl) && al.size()==bl.size()){
+            ans=true;
+        }
+        else{
+            ans=false;
+        }
+        return ans;
+        
+    }
+}
