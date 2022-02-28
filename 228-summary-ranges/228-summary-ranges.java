2657865d75fdaf36115412369026813156d77640
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-03-01 00:19:52 +0530

    Time: 11 ms (29.44%), Space: 42.9 MB (5.82%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        int[] nums1=new int[nums.length+1];
+        for(int i=0;i<nums1.length;i++){
+            if(i==nums1.length-1){
+                nums1[i]=10000;
+            }
+            else{
+                nums1[i]=nums[i];
+            }
+        }
+        List<String> l=new ArrayList();
+        String s="";
+        int count=0;
+        for(int i=0;i<nums1.length-1;i++){
+            
+            
+            if(nums1[i]+1==nums1[i+1]){
+                if(count==0){
+                    s=s+nums1[i]+"->";
+                }
+                count++;
+                continue;
+                
+            }
+            else{
+                s=s+nums1[i];
+                l.add(s);
+                count=0;
+                s="";
+                
+            }
+            // l.add(s);
+            
+        }
+        
+        
+        return l;
+    }
+}
