af99bbf23ebf51373f9beb38b30923f2819abb5d
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-05-27 00:04:50 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (82.40%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -4,7 +4,7 @@ public class Solution {
         int count=0;
         while(n!=0){
             ++count;
-            n=n&(n-1);
+            n=n&(n-1);//right most bit set bit( is set to 0 ..... i.e. 0101010 --> 0101000)
         }
         
         return count;
