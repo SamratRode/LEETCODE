1de22fe794dd62a6a1304cb48c75054d84071446
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-06-11 00:50:27 +0530

    Time: 303 ms (7.18%), Space: 101.7 MB (9.84%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,17 +5,17 @@ class Solution {
         if(s.length()==0){
             return 0;
         }
-        char[] ch=new char[s.length()];
-        for(int k=0;k<s.length();k++){
-            ch[k]=s.charAt(k);
-        }
+        // char[] ch=new char[s.length()];
+        // for(int k=0;k<s.length();k++){
+        //     ch[k]=s.charAt(k);
+        // }
         int max=1;
         HashSet<Character> hm=new HashSet();
-        while(j<s.length() && i<s.length()){
+        while(j<s.length()){
             
-            if(ch[i]!=ch[j] && !hm.contains(ch[j])){
-                hm.add(ch[i]);
-                hm.add(ch[j]);
+            if(!hm.contains(s.charAt(j))){
+                hm.add(s.charAt(i));
+                hm.add(s.charAt(j));
                 if(hm.size()>max){
                     max=hm.size();
                 }
