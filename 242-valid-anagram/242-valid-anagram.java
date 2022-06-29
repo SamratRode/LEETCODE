e2fba2c1b9a28195c30a165bf0e2df3966b7651f
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-06-30 02:22:55 +0530

    Time: 49 ms (5.12%), Space: 46.5 MB (20.48%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        boolean ans=true;
-        ArrayList al=new ArrayList();
-        al.addAll(Arrays.asList(s.split("")));
-        ArrayList bl=new ArrayList();
-        bl.addAll(Arrays.asList(t.split("")));
-        Collections.sort(al);
-        Collections.sort(bl);
-        /*System.out.println(al + " " + bl);*/
-        if(al.equals(bl)){
-            ans=true;
+        if(s.length()!=t.length()){
+            return false;
         }
-        else{
-            ans=false;
+        TreeMap<Character,Integer> tm=new TreeMap<Character,Integer>();
+        TreeMap<Character,Integer> tm1=new TreeMap<Character,Integer>();
+        for(int i=0;i<s.length();i++){
+            tm.put(s.charAt(i),tm.getOrDefault(s.charAt(i),0)+1);
+            tm1.put(t.charAt(i),tm1.getOrDefault(t.charAt(i),0)+1);
         }
-        return ans;
+        if(tm.equals(tm1)){
+            return true;
+        }
+        return false;
         
     }
 }
