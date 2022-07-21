4b500f90e1e3e1d2b4834df74adf8ee1df75335f
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-07-21 15:26:33 +0530

    Time: 6 ms (57.99%), Space: 43.7 MB (65.02%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ll=new LinkedList<>();
         Arrays.sort(candidates);
-        
-        combinations(0,candidates,target,ll,new ArrayList<>());
+        List<List<Integer>> ll=new LinkedList<>();
+        combinations(0,new ArrayList<>(),ll,target,candidates);
         return ll;
     }
-    public void combinations(int ind,int[] candidates,int target,List<List<Integer>> ll,List<Integer> ds){
+    public void combinations(int ind,List<Integer> l,List<List<Integer>> ll,int target,int[] candidates ){
         if(target==0){
-            ll.add(new ArrayList<>(ds));
+            ll.add(new ArrayList<>(l));
             return;
         }
         for(int i=ind;i<candidates.length;i++){
             if(i>ind && candidates[i]==candidates[i-1]){
                 continue;
             }
-            if(candidates[i]>target){
+            else if(candidates[i]>target){
                 break;
             }
-            ds.add(candidates[i]);
-            combinations(i+1,candidates,target-candidates[i],ll,ds);
-            ds.remove(ds.size()-1);
-        
+            l.add(candidates[i]);
+            combinations(i+1,l,ll,target-candidates[i],candidates);
+            l.remove(l.size()-1);
         }
+         
     }
 }
