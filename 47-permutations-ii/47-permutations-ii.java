109c6a6c842e86f30441b3185a7580744daf41fa
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-07-25 16:40:01 +0530

    Time: 4 ms (73.99%), Space: 48 MB (60.57%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,28 +1,27 @@
-class Solution {
+public class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        HashSet<List<Integer>> hl=new HashSet<>();
-        List<List<Integer>> ll=new LinkedList<>();
-        List<Integer> ds=new LinkedList<>();
-        int[] map=new int[nums.length];
-        f(ds,map,hl,nums);
-        for(List<Integer> lis:hl){
-            ll.add(lis);
-        }
-        return ll;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums==null || nums.length==0) return res;
+        boolean[] used = new boolean[nums.length];
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, used, list, res);
+        return res;
     }
-    public void f(List<Integer> ds,int[] map,HashSet<List<Integer>> hl,int[] nums){
-        if(ds.size()==nums.length){
-            hl.add(new LinkedList<>(ds));
+
+    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<Integer>(list));
             return;
         }
         for(int i=0;i<nums.length;i++){
-            if(map[i]!=-1){
-                ds.add(nums[i]);
-                map[i]=-1;
-                f(ds,map,hl,nums);
-                ds.remove(ds.size()-1);
-                map[i]=0;
-            }
+            if(used[i]) continue;
+            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;
+            used[i]=true;
+            list.add(nums[i]);
+            dfs(nums,used,list,res);
+            used[i]=false;
+            list.remove(list.size()-1);
         }
     }
 }
