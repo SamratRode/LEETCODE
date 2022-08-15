49692af68b07d56031d9a6eea85b6905701e7f08
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-08-15 20:10:24 +0530

    Time: 5 ms (34.96%), Space: 43 MB (24.84%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,32 +1,30 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        //trying with the sliding window
         int i=0;
         int j=0;
         int count=0;
-        ArrayList<Character> al=new ArrayList<Character>();
+        HashMap<Character,Integer> hm=new HashMap<Character,Integer>();
         while(j<s.length()){
-            al.add(s.charAt(j));
+            hm.put(s.charAt(j),hm.getOrDefault(s.charAt(j),0)+1);
             if(j-i+1<3){
                 j++;
             }
             else if(j-i+1==3){
-                if(isUnique(al)){
-                    // System.out.println(al);
+                if(hm.size()==3){
                     count++;
                 }
-                al.remove(0);
+                int val=hm.get(s.charAt(i));
+                val=val-1;
+                if(val==0){
+                    hm.remove(s.charAt(i));
+                }
+                else{
+                    hm.put(s.charAt(i),val);
+                }
                 i++;
                 j++;
             }
         }
         return count;
-        
-    }
-    public boolean isUnique(ArrayList<Character> al){
-        if(al.get(0)==al.get(1) || al.get(1)==al.get(2) || al.get(0)==al.get(2)){
-            return false;
-        }
-        return true;
     }
 }
