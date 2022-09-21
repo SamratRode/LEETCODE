5ba2d9053d6da102dbd1b5bb90fa9a9886c57517
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2022-09-21 13:56:33 +0530

    Time: 63 ms (14.94%), Space: 63.9 MB (46.75%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -1,25 +1,15 @@
 class Solution {
-    public List<String> findRepeatedDnaSequences(String s) {
-        HashMap<String,Integer> hm=new HashMap<>();
-        List<String> res=new ArrayList<>();
-        for(int i=0;i<s.length();i=i+1){
-            int a=i;
-            String x="";
-            for(int j=i;j<a+10 && j<s.length();j++){
-                x=x+s.charAt(j);
-            }
-            if(x.length()==10){
-                hm.put(x,hm.getOrDefault(x,0)+1);
-            }
-        }
-        // System.out.println(hm);
-        for(String b:hm.keySet()){
-            if(hm.get(b)>1){
-                res.add(b);
-            }
-            
-        }
-        return res;
-        
+
+public List<String> findRepeatedDnaSequences(String s) {
+    HashMap<String,Integer> hm = new HashMap<>();
+    List<String> ans = new ArrayList<>();
+    for(int i=0;i<=s.length()-10;i++){
+        String tem = s.substring(i,i+10);
+        hm.put(tem,hm.getOrDefault(tem,0)+1);
     }
+    for(String str:hm.keySet())
+        if(hm.get(str)>1)
+            ans.add(str);
+    return ans;
+}
 }
